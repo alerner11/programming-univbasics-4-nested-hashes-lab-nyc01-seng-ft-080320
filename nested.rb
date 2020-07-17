5a6ef17978ec0fdf28150a4f971048c53d13cc44
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-17 17:47:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,6 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+  programmer_hash[:dennis_ritchie][:languages]
 
 end
 
@@ -83,6 +84,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
+  programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages: ["LISP", "C"]
+  }
 
 end
 
